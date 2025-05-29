778b9171b90f68839f04bf00f193b999f15e6c47
Ксения <kssshaab@gmail.com>
2025-05-29 20:37:13 +0300

    get_mask

---
+++ src/masks.py
@@ -0,0 +1,35 @@
+user_input_card_number = input()
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскирует номер карты"""
+    card_number_str = str(card_number).replace(" ", "")
+    if len(card_number_str) != 16 or not card_number_str.isdigit():
+        return "Неверный номер карты"
+
+    first_six_digits = card_number_str[0:6]
+    last_four_digits = card_number_str[-4:]
+
+    masked_card_number = first_six_digits[:4] + " " + first_six_digits[4:6] + "** ****" + " " + last_four_digits
+    return masked_card_number
+
+
+print(get_mask_card_number(user_input_card_number))
+
+
+user_input_account = input()
+
+
+def get_mask_account(account: str) -> str:
+    """Функция маскирует номер счета"""
+    account_str = str(account).replace(" ", "")
+    if len(account_str) != 20 or not account_str.isdigit():
+        return "Неверный номер счета"
+
+    last_four_digits = account_str[-4:]
+
+    masked_account = "**" + last_four_digits
+    return masked_account
+
+
+print(get_mask_account(user_input_account))
