8d9ede80a16bc716be393f6319ef2144503d826b
Ксения <kssshaab@gmail.com>
2025-07-18 13:45:43 +0300

    add test_read_transactions

--- src/read_transactions.py
+++ src/read_transactions.py
@@ -1,24 +1,34 @@
 import csv
+from typing import Dict, List
+
 import pandas as pd
-from typing import List, Dict
+
 
 def read_transactions_from_csv(csv_file: str) -> List[Dict[str, str]]:
     """Считывает финансовые операции из CSV-файла и возвращает список словарей"""
     transactions_csv = []
-    with open(csv_file, newline='', encoding='utf-8') as file:
-        reader = csv.DictReader(file, delimiter=';')
+    with open(csv_file, newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=";")
         for row in reader:
             transactions_csv.append(row)
-    return  transactions_csv
+    return transactions_csv
+
 
 if __name__ == "__main__":
-    csv_data = '../data/transactions.csv'
+    csv_data = "../data/transactions.csv"
     transactions = read_transactions_from_csv(csv_data)
     for transaction in transactions:
-        print(transaction['id'], transaction['state'],
-              transaction['date'], transaction['amount'],
-              transaction['currency_name'], transaction['currency_code'],
-              transaction['from'], transaction['to'], transaction['description'])
+        print(
+            transaction["id"],
+            transaction["state"],
+            transaction["date"],
+            transaction["amount"],
+            transaction["currency_name"],
+            transaction["currency_code"],
+            transaction["from"],
+            transaction["to"],
+            transaction["description"],
+        )
 
 
 def read_transactions_from_excel(excel_file: str) -> List[Dict[str, str]]:
@@ -29,11 +39,19 @@ def read_transactions_from_excel(excel_file: str) -> List[Dict[str, str]]:
         transactions_excel.append(row.to_dict())
     return transactions_excel
 
+
 if __name__ == "__main__":
-    excel_data = '../data/transactions_excel.xlsx'
-    transactions = read_transactions_from_excel(excel_data)
+    excel_dir = "../data/transactions_excel.xlsx"
+    transactions = read_transactions_from_excel(excel_dir)
     for transaction in transactions:
-        print(transaction['id'], transaction['state'],
-              transaction['date'], transaction['amount'],
-              transaction['currency_name'], transaction['currency_code'],
-              transaction['from'], transaction['to'], transaction['description'])
+        print(
+            transaction["id"],
+            transaction["state"],
+            transaction["date"],
+            transaction["amount"],
+            transaction["currency_name"],
+            transaction["currency_code"],
+            transaction["from"],
+            transaction["to"],
+            transaction["description"],
+        )
